350eed053a9d6687d7bbccaeeb2638cf9be47bd4
Vaibhav Khamesra <vaibhavkhamesra643@gmail.com>
2026-05-24 18:54:39 +0530

    feat: add Hash256 type and sha256 utilities

---
+++ src/primitives/hash.rs
@@ -0,0 +1,16 @@
+use sha2::{Digest, Sha256};
+
+pub type Hash256 = [u8; 32];
+
+pub fn zero_hash() -> Hash256{
+    [0u8; 32]
+}
+
+pub fn sha256(data: &[u8]) -> Hash256{
+    let digest = Sha256::digest(data);
+
+    let mut hash = [0u8; 32];
+    hash.copy_from_slice(&digest);
+
+    hash
+}
