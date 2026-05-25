acf30e06c3e07fb7a849c4e471b14ce4785c38ed
Vaibhav Khamesra <vaibhavkhamesra643@gmail.com>
2026-05-25 13:58:03 +0530

    chore: update lib.rs and test blockchain creation in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use minibitcoin::primitives::{block::Block, hash::hash_to_hex};
+use minibitcoin::chain::blockchain::Blockchain;
 
 fn main() {
     let genesis_block = Block::genesis();
@@ -20,6 +21,20 @@ fn main() {
     
     println!("{:?}", block2.hash());
     println!("{}", hash_to_hex(&block2.hash()));
-    
-    
+
+    println!("------------------------------------");
+
+    let mut blockchain = Blockchain::new();
+    blockchain.add_block();
+    blockchain.add_block();
+    blockchain.add_block();
+    blockchain.add_block();
+    blockchain.add_block();
+    blockchain.add_block();
+    for (i, block) in blockchain.blocks.iter().enumerate(){
+        println!("Block: {i}");
+        println!("Block Hash: {}",hash_to_hex(&block.hash()));
+        println!("Previous Block Hash: {}",hash_to_hex(&block.header.previous_hash));
+        println!("-----------------------");
+    }
 }
