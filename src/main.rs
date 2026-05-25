9a3da9d50585d94cced1dd83a2e8d9bc82e3d121
Vaibhav Khamesra <vaibhavkhamesra643@gmail.com>
2026-05-25 17:44:53 +0530

    chore: verify chain validation

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,6 @@ use minibitcoin::primitives::{block::Block, hash::hash_to_hex};
 use minibitcoin::chain::blockchain::Blockchain;
 
 fn main() {
-    let genesis_block = Block::genesis();
-    let hash = genesis_block.hash();
-    println!("{:?}", hash);
-    println!("{}", hash_to_hex(&hash));
-
-    let block1 = Block::next(&genesis_block);
-    let block2 = Block::next(&block1);
-
-    println!("{:?}", block1.header.previous_hash);
-    println!("{}", hash_to_hex(&block1.header.previous_hash));
-
-    println!("{:?}", block1.hash());
-    println!("{}", hash_to_hex(&block1.hash()));
-
-    println!("{:?}", block2.header.previous_hash);
-    println!("{}", hash_to_hex(&block2.header.previous_hash));
-    
-    println!("{:?}", block2.hash());
-    println!("{}", hash_to_hex(&block2.hash()));
-
-    println!("------------------------------------");
 
     let mut blockchain = Blockchain::new();
     blockchain.add_block();
@@ -39,4 +18,7 @@ fn main() {
     }
 
     println!("{}", blockchain.validate_chain());
+    blockchain.blocks[2].header.nonce = 999;
+    println!("{}", blockchain.validate_chain());
+    
 }
